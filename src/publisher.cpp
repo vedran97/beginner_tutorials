d318556088b23157b1cff3aef19ff15d08db86bf
VEDANT RANADE <vedantmranade@gmail.com>
2023-11-02 23:56:58 -0400

    Added a publisher
    Added a ros2 publisher which publishes a custom message on a custom topic

--- src/publisher.cpp
+++ src/publisher.cpp
@@ -1,9 +1,35 @@
 #include <chrono>
 #include <functional>
 #include <memory>
+#include <rclcpp/executors.hpp>
+#include <rclcpp/timer.hpp>
 #include <string>
+#include <std_msgs/msg/string.hpp>
 
-#include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+using std::placeholders::_1;
 
-int main() { return 0; }
+class StringPublisher : public rclcpp::Node {
+ public:
+  StringPublisher() : Node("string_publisher") {
+    publisher_ =
+        this->create_publisher<std_msgs::msg::String>("Problem_Pub", 10);
+    timer_ =
+        this->create_wall_timer(std::chrono::milliseconds(1000),
+                                std::bind(&StringPublisher::callback, this));
+  }
+
+ private:
+  void callback() {
+    auto message = std_msgs::msg::String();
+    message.data = "Theres a problem houston";
+    publisher_->publish(message);
+  }
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+};
+int main(int argc, char* argv[]) {
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<StringPublisher>());
+  rclcpp::shutdown();
+  return 0;
+}
